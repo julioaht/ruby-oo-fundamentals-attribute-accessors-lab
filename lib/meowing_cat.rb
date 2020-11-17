fa66a1bc4eab665b2ebbc8849f61f5b6a33128c0
Julio Hernandez <julioandres72@gmail.com>
2020-11-17 10:10:56 -0500

    done

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,8 +1,34 @@
-## code your solution here. 
 class Cat
-    attr_accessor :name
-    
-    def meow
-        puts "meow!"
-      end
+  attr_accessor :name
+
+
+  def meow
+    puts 'meow!'
+  end
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ## code your solution here. 
+# class Cat
+#     attr_accessor :name
+    
+#     def meow
+#         puts "meow!"
+#       end
+# end
